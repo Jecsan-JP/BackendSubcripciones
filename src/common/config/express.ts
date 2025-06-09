4d6c76b498d863e956aec464a64705842567ef63
Jecsan Ortega <jecsan.jeck@gmail.com>
2025-06-09 12:42:28 -0600

    Inicializacion del proyecto
    *Se instalaron las dependencias correspondientes
    *Se estructuraron las carpetas manteniendo clean arquitecture

---
+++ src/common/config/express.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { errorHandler } from "../middlewares/errorHandler.middleware";
+import { responseMiddleware } from "../middlewares/response.middleware";
+
+// import routes from "../routes";
+
+export const createExpressApp = () => {
+  const app = express();
+
+  // Middlewares
+  app.use(express.json());
+  app.use(cors());
+  app.use(morgan("dev"));
+
+  // Formateador de Respuestas
+  app.use(responseMiddleware);
+
+  //Rutas
+  app.use("/api", routes);
+  app.use(
+    (
+      err: Error,
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction
+    ) => {
+      errorHandler(err, req, res, next);
+    }
+  );
+
+  return app;
+};
